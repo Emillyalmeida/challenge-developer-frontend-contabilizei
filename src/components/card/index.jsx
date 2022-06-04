6a58dc65e11c636c4d7cebdac808ae5903ee1875
emillyalmeida <emillyalmeidan@gmail.com>
2022-06-03 22:59:49 -0300

    feat(card): create card and add style

---
+++ src/components/card/index.jsx
@@ -0,0 +1,14 @@
+import { CardLi } from "./style";
+
+const Card = ({ hq }) => {
+  const { title, images, prices } = hq;
+  return (
+    <CardLi>
+      <img src={`${images[0].path}.${images[0].extension}`} alt={title} />
+      <h3>{title}</h3>
+      <span>Price: US$ {prices[0].price}</span>
+    </CardLi>
+  );
+};
+
+export default Card;
---
+++ src/components/card/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const CardLi = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 280px;
+  height: 330px;
+  margin: 10px;
+
+  img {
+    width: 250px;
+    height: 280px;
+  }
+`;
