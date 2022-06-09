57bd0bc944adb6a080030e232097809a03352b92
emillyalmeida <emillyalmeidan@gmail.com>
2022-06-08 22:58:53 -0300

    feat(checkout): add tags html and init style

--- src/pages/checkout/index.jsx
+++ src/pages/checkout/index.jsx
@@ -1,13 +1,48 @@
 import Container from "../../components/container";
-import { DetailsDemand, LogoHeader } from "./style";
+import { DetailsDemand, LogoHeader, Main } from "./style";
+import { useContext } from "react";
+import { CartContext } from "../../providers/cart";
 
 const Checkout = () => {
+  const { Cart } = useContext(CartContext);
+
   return (
     <Container>
       <LogoHeader>
         <h1> House of Marvel</h1>
       </LogoHeader>
-      <DetailsDemand></DetailsDemand>
+      <Main>
+        <DetailsDemand>
+          <h2> Details of the demand</h2>
+          <section>
+            <div>
+              <span>SubTotal</span>
+              <span>
+                R${" "}
+                {Cart.reduce(
+                  (sum, curr) => curr.prices[0].price * curr.qtd + sum,
+                  0
+                )
+                  .toFixed(2)
+                  .replace(".", ",")}
+              </span>
+            </div>
+            <div>
+              <span>Desconto</span>
+              <span>R$ 0,00</span>
+            </div>
+            <div>
+              <h3>Total</h3>
+              <h3>R$</h3>
+            </div>
+          </section>
+          <div>
+            <input type="text" placeholder="digite seu cupom" />
+            <button>aplicar</button>
+          </div>
+          <button>Confirm</button>
+        </DetailsDemand>
+      </Main>
     </Container>
   );
 };
--- src/pages/checkout/style.js
+++ src/pages/checkout/style.js
@@ -1,4 +1,16 @@
 import styled from "styled-components";
+import imgCheckout from "../../assets/imageCheckout.jpg";
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  height: 100%;
+  background-image: url(${imgCheckout});
+  background-position: right;
+  overflow-y: auto;
+`;
 
 export const LogoHeader = styled.header`
   background-color: var(--color-main);
@@ -24,8 +36,60 @@ export const DetailsDemand = styled.section`
   height: auto;
   position: relative;
   @media (min-width: 768px) {
-    flex-direction: row;
     width: 80%;
+    max-width: 500px;
     padding: 20px;
+  }
+
+  h2 {
+    color: var(--color-main);
+    padding-bottom: 2rem;
+  }
+
+  > section {
+    padding: 0 15px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 1rem;
+
+    > div {
+      display: flex;
+      justify-content: space-between;
+      padding-bottom: 3px;
+    }
+  }
+
+  > div {
+    padding: 0 15px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    padding-bottom: 1rem;
+
+    input {
+      flex: 1;
+      height: 35px;
+      border-radius: 5px;
+    }
+
+    button {
+      margin-left: 1rem;
+      width: 70px;
+      height: 35px;
+      border: none;
+      border-radius: 5px;
+      background-color: var(--color-main);
+      color: var(--color-hover);
+    }
+  }
+
+  button {
+    width: 90%;
+    height: 40px;
+    border: none;
+    border-radius: 5px;
+    background-color: var(--color-main);
+    color: var(--color-hover);
   }
 `;
